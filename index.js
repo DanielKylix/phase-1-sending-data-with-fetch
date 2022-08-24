ed810406da931e50667243d9793fde5e4a5c023a
Daniel <daniel@kylix.online>
2022-08-24 11:25:36 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+const submitData = async (name,email) => {
+    const configurationObject = {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(name,email),
+      };
+      
+      await fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (object) {
+          console.log(object);
+        })
+        .catch(function (error) {
+          alert("Bad things! Ragnarők!");
+          console.log(error.message);
+        });
+}
